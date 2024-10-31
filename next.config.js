32710f0482c47022d9f79ba9fe9c3402d5efd010
meryaaam <mariemchouaiti@gmail.com>
2024-10-31 22:18:04 +0100

    feat: add docker config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: true,
+    output: 'standalone' , 
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'http',
+          hostname: 'localhost',
+        },
+         
+      ],
+    },
+  };
+  
+  module.exports = nextConfig;
+  
