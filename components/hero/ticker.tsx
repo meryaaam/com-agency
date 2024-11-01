1c9c6523717b2a125158695078470ddf6ba46d94
meryaaam <mariemchouaiti@gmail.com>
2024-11-01 22:53:10 +0100

    feat : add ticker

---
+++ components/hero/ticker.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "@nextui-org/button";
+
+export const Ticker = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <section></section>
+  );
+};
