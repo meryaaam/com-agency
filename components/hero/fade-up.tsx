124e0ac61c958e30895447218698ab765ec88b2d
meryaaam <mariemchouaiti@gmail.com>
2024-11-02 13:24:41 +0100

    refcator : Refactor hero component: replaced hardcoded title styles with `title` primitive, simplified title layout, and rearranged elements.

--- components/hero/fade-up.tsx
+++ components/hero/fade-up.tsx
@@ -6,6 +6,7 @@ import { Button, Card , CardBody, CardFooter, CardHeader, Image, Link } from "@n
 import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { RightIcon, StarIcon } from "../icons";
+import { title } from "../primitives";
 
 const FadeUp = () => { 
     const [isCardVisible, setIsCardVisible] = useState(false);
@@ -20,20 +21,17 @@ const FadeUp = () => {
    <section className="relative z-10 flex flex-col gap-2 w-full mt-24 lg:mt-56">
      <div className="flex flex-col gap-8">
         <div>
-            <div className="flex flex-col gap-2 items-start justify-center w-full">
-                <h1 className="tracking-tight inline font-semibold text-4xl lg:text-6xl">The Loud</h1>
-                 <div>
-                    <h1 className="tracking-tight inline font-semibold from-[#5EA2EF] to-[#0072F5] text-4xl lg:text-6xl bg-clip-text text-transparent bg-gradient-to-b">
-                        Voice&nbsp;
-                    </h1>
-                    <h1 className="tracking-tight inline font-semibold text-4xl lg:text-6xl">of your Brand.</h1>
-                </div>
-            </div>
-                    <p className="w-full md:w-1/2 my-2 text-lg lg:text-xl font-normal text-default-500 block max-w-full">
-                    NextUI provides a custom TailwindCSS plugin that allows you to customize the default themes or create your own.
-                    </p>
+            
+            <span className={title({size:"lg"})}>The Loud&nbsp;</span>
+            <span className={title({size:"lg", color: "blue"})}>  Voice &nbsp; </span> <br />
+        
+        <span className={title({ size:"lg"  })}>of your Brand &nbsp;</span>
         </div>
-        </div>
+        <p className="w-full md:w-1/2 my-2 text-lg lg:text-xl font-normal text-default-500 block max-w-full">
+            NextUI provides a custom TailwindCSS plugin that allows you to customize the default themes or create your own.
+            </p>
+        </div> 
+        
 </section>
 );
 };
