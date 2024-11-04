c805a7b01271899689713741aa0b011ebd5348f8
meryaaam <mariemchouaiti@gmail.com>
2024-11-04 23:06:56 +0100

    refactor : Added key property to services array, imported additional Framer Motion functions, and implemented scroll-based animation and visibility for services cards

--- components/hero/services.tsx
+++ components/hero/services.tsx
@@ -1,13 +1,14 @@
 "use client";
 import React, { useState, useRef, useEffect } from "react";
 import { subtitle, title } from "../primitives";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion, useIsPresent, useScroll, useTransform } from "framer-motion";
 import SectionLayout from "@/common/SectionLayout";
 import HorizontalWrapper from "@/common/HorizontalWrapper";
 import { Card, CardBody, CardHeader, Image } from "@nextui-org/react";
 
 const services = [
   {
+    key : "MK-01" , 
     title: 'Marketing',
     shortDescription : "Engage, Inspire, Drive Success." ,
     description : "Tailored strategies that boost your brand's visibility and connect with your audience like never before." ,
@@ -15,6 +16,7 @@ const services = [
     imageUrl: '/images/1.jpg',
   },
   {
+    key : "BG-01" , 
     title: 'Branding',
     shortDescription : "Stand Out, Be Memorable." ,
     description : "Elevate your identity—bold, memorable, and unmistakably you." ,
@@ -22,6 +24,7 @@ const services = [
     imageUrl: '/images/1.jpg',
   },
   {
+    key : "DG-01" , 
     title: 'Design',
     shortDescription : "Creative Designs That Captivate." ,
     description : "Designs that don’t just catch the eye, but captivate the mind and inspire action."
@@ -29,13 +32,16 @@ const services = [
     text : "Crafting unique identities that resonate and make lasting impressions in a crowded market." ,
     imageUrl: '/images/1.jpg',
   },
-  { title: 'SEO',
+  { 
+    key : "SEO-01" , 
+    title: 'SEO',
     shortDescription : "Boost Visibility, Dominate Search." ,
     description : "Be found, be chosen—SEO that puts your brand on the digital map and keeps it there." ,
     text : "Precision-driven SEO services that enhance your online presence and push you to the top of search results." ,
     imageUrl: '/images/1.jpg'} 
     ,
-  { title: 'Developement',
+  {  key : "DEV-01" , 
+    title: 'Developement',
     shortDescription : "Innovative Solutions, Seamless Performance." ,
     description : "Innovative digital experiences, seamless performance, and platforms that keep your users coming back for more." ,
     text : "Cutting-edge, user-friendly web solutions that are built to engage and perform." ,
@@ -44,15 +50,58 @@ const services = [
   
   // Add more sections as needed
 ];
+type VisibleCards = {
+  [key: string]: boolean;  
+};
 
 export const Services = () => {
- 
-  useEffect(() => {
+  const [visibleCards, setVisibleCards] = useState<VisibleCards>({}); // Set the initial state type
+
+  const scrollRef = useRef<HTMLDivElement | null>(null); // Set type for ref
+  const { scrollYProgress } = useScroll({
+      target: scrollRef,
+      offset: ['start start', 'end end']
+
   })
+  useEffect(() => {
+    const handleScroll = () => {
+      const newVisibleCards: VisibleCards = {};
+      const scrollY = window.scrollY;
+      const scrollContainer = scrollRef.current;
+   
+      services.forEach((item, index) => {
+        const triggerPoint = (index + 1) * 300;  
+        newVisibleCards[index] = scrollY > triggerPoint && scrollY < triggerPoint + 300; 
+      });
+
+      setVisibleCards(newVisibleCards);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    handleScroll(); 
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+
+    
+  }, []);
+  const isPresent = useIsPresent()
 
   return (
-    <section className=" grid grid-rows-5 w-full gap-6  ">
-  {services.map((item) => (
+    <section ref={scrollRef} className="grid grid-rows-5 w-full gap-6  ">
+     
+
+  {isPresent && services.map((item,key) => (
+   <AnimatePresence key={key}>
+      {visibleCards[key] && (
+       <motion.div
+     id={item.key}
+     initial={{ opacity: 0.5, y: 40 }}
+     animate={visibleCards[key] ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }} 
+     exit={{ opacity:  -300 }}
+     transition={{ duration: 0.6 }}
+   >
       <Card
         isBlurred
         className="border-none bg-background/60 dark:bg-default-100/50 max-auto"
@@ -60,7 +109,6 @@ export const Services = () => {
       >
         <CardHeader>
           <div className="relative col-span-6 md:col-span-4">
-          
           <div className={subtitle({color:"blue" , class: "w-full text-lg font-bold lg:text-xl max-w-xl mt-4 mx-auto" })}>
           {item.shortDescription}
           </div>
@@ -83,7 +131,10 @@ export const Services = () => {
           </div>
         </CardBody>
       </Card>
+      </motion.div>)}
+    </AnimatePresence>
   ))}
+
      </section>
   );
 };
