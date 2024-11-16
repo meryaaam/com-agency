47143a90232c8f66a0a592f397f667d37f18d1f7
meryaaam <mariemchouaiti@gmail.com>
2024-11-16 21:36:37 +0100

    refactore : about page

--- components/common/home/services.tsx
+++ components/common/home/services.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useRef, useEffect } from "react";
-import { subtitle, title } from "../primitives";
+import { subtitle, title } from "../../primitives";
 import { AnimatePresence, motion, useIsPresent, useScroll, useTransform } from "framer-motion"; 
 import { Card, CardBody, CardHeader, Image } from "@nextui-org/react";
 import ReactLenis from "@studio-freight/react-lenis";
