32a8110c2ef3feb2fb5148fb126ec0da1c401237
meryaaam <mariemchouaiti@gmail.com>
2024-11-30 12:49:25 +0100

    refctor : Added 3D scene with lights and physics to Tag3d component

--- components/animations/3dtag.tsx
+++ components/animations/3dtag.tsx
@@ -29,7 +29,43 @@ useTexture.preload(
 
 export default function Tag3d() {
   return (
-    <></>
+    <Canvas camera={{ position: [0, 0, 12], fov: 25 }}>
+      <ambientLight intensity={Math.PI} />
+      <Physics interpolate gravity={[0, -40, 0]} timeStep={1 / 60}>
+         
+      </Physics>
+      <Environment background blur={0.75}>
+        <color attach="background" args={["black"]} />
+        <Lightformer
+          intensity={2}
+          color="white"
+          position={[0, -1, 5]}
+          rotation={[0, 0, Math.PI / 3]}
+          scale={[100, 0.1, 1]}
+        />
+        <Lightformer
+          intensity={3}
+          color="white"
+          position={[-1, -1, 1]}
+          rotation={[0, 0, Math.PI / 3]}
+          scale={[100, 0.1, 1]}
+        />
+        <Lightformer
+          intensity={3}
+          color="white"
+          position={[1, 1, 1]}
+          rotation={[0, 0, Math.PI / 3]}
+          scale={[100, 0.1, 1]}
+        />
+        <Lightformer
+          intensity={10}
+          color="white"
+          position={[-10, 0, 14]}
+          rotation={[0, Math.PI / 2, Math.PI / 3]}
+          scale={[100, 10, 1]}
+        />
+      </Environment>
+    </Canvas>
   );
 }
 
