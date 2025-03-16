1451626fa8f788995259543d3f81179eb8c57e65
meryaaam <mariemchouaiti@gmail.com>
2025-03-16 05:18:05 +0100

    chore : Update dependencies and configuration files

--- components/common/works.tsx
+++ components/common/works.tsx
@@ -8,34 +8,34 @@ import { button as buttonStyles } from "@nextui-org/theme";
 import { title } from "../primitives";
 
 interface ImageItem {
-  img: string;
+  //img: string;
   label: string;
   description: string;
 }
 const list: ImageItem[] = [
   {
-    img: "https://images.unsplash.com/photo-1682806816936-c3ac11f65112?q=80&w=1274&auto=format&fit=crop",
+    //img: "https://images.unsplash.com/photo-1682806816936-c3ac11f65112?q=80&w=1274&auto=format&fit=crop",
     label: "Revamping User Experiences",
     description: " A Web Design Overhaul for Digital Growt",
   },
   {
-    img: "https://images.unsplash.com/photo-1681063762354-d542c03bbfc5?q=80&w=1274&auto=format&fit=crop",
+    //img: "https://images.unsplash.com/photo-1681063762354-d542c03bbfc5?q=80&w=1274&auto=format&fit=crop",
     label: "From Concept to Canvas",
     description: "Crafting Visual Identities that Stand Out",
   },
   {
-    img: "https://images.unsplash.com/photo-1679640034489-a6db1f096b70?q=80&w=1274&auto=format&fit=crop",
+    //img: "https://images.unsplash.com/photo-1679640034489-a6db1f096b70?q=80&w=1274&auto=format&fit=crop",
     label: "Climbing the Ranks",
     description: "An SEO Project That Drives Organic Growth",
   },
   {
-    img: "https://images.unsplash.com/photo-1679482451632-b2e126da7142?q=80&w=1274&auto=format&fit=crop",
+    //img: "https://images.unsplash.com/photo-1679482451632-b2e126da7142?q=80&w=1274&auto=format&fit=crop",
     label: "Maximizing ROI",
     description:
       "Comprehensive Digital Marketing Campaigns for Business Success",
   },
   {
-    img: "https://images.unsplash.com/photo-1679482451632-b2e126da7142?q=80&w=1274&auto=format&fit=crop",
+    //img: "https://images.unsplash.com/photo-1679482451632-b2e126da7142?q=80&w=1274&auto=format&fit=crop",
     label: "Complete Digital Transformation",
     description: "A Holistic Approach to Web Design, SEO, and Marketing",
   },
@@ -79,12 +79,12 @@ function ImageReveal2() {
   };
 
   const handleImageInteraction = (item: ImageItem, opacity: number) => {
-    setImg({ src: item.img, alt: item.label, opacity });
+    setImg({ src: "item.img", alt: item.label, opacity });
   };
 
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div ref={containerRef} onMouseMove={handleMove} className="relative ">
+      <div  className="relative ">
         <div className="grid grid-cols-2 px-8 mb-5 gap-5">
           <div className=" ">
             <span className={title({ size: "md", color: "green" })}>
@@ -100,9 +100,9 @@ function ImageReveal2() {
         {list.map((item) => (
           <div
             key={item.label}
-            onMouseEnter={() => handleImageInteraction(item, 1)}
-            onMouseMove={() => handleImageInteraction(item, 1)}
-            onMouseLeave={() => handleImageInteraction(item, 0)}
+            //onMouseEnter={() => handleImageInteraction(item, 1)}
+            //onMouseMove={() => handleImageInteraction(item, 1)}
+            //onMouseLeave={() => handleImageInteraction(item, 0)}
             className="w-full py-5 cursor-pointer  text-center flex justify-between border-b last:border-none"
           >
             <p
@@ -140,7 +140,7 @@ function ImageReveal2() {
           </div>
         ))}
 
-        <motion.img
+        {/*<motion.img
           ref={imageRef}
           src={img.src}
           alt={img.alt}
@@ -150,7 +150,7 @@ function ImageReveal2() {
             y: imagePos.y,
             opacity: img.opacity,
           }}
-        />
+        />*/}
       </div>
     </section>
   );
